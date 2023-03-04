b679d46672663216e2e4a4d30ec8cae73d4d3ae7
Rodrigo Vian <rodrigovian@gmail.com>
2023-03-04 14:09:49 -0300

    Bug Added

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)
+
 @app.route("/")
 def pagina_inicial():
     return "Just Rock"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
+if __name__ == '__main__':
+    app.run()
